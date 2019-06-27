09ea4927bcb15e66647e1267c2782860f2880586
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-27 14:38:23 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,9 +22,9 @@ def bonus
 
   #code your solution here:
 
-hash = {first: "first value!", second: "second value!"}
- 
-hash[:first]
+  hash = {first: "first value!", second: "second value!"}
+   
+  hash[:first]
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
